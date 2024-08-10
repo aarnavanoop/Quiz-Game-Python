19acc3ac8463421101ebe682d88a40477ee93b20
aarnavanoop <aarnavanoop@gmail.com>
2024-08-10 12:00:39 +1000

    Finishing the start to the quiz

--- quiz.py
+++ quiz.py
@@ -1,8 +1,15 @@
 #Welcome user to the game
+print("Welcome to my NBA Quiz! ")
 
 #check if user wants to play the game
+interest = input("Would you like to try the quiz? ").lower()
 
 #quit if user does not want to play the game
+if interest != "yes":
+    quit()
+
+#begin the quiz
+print("Lets Begin! ")
 
 #Ask 4 questions to the user 
 
